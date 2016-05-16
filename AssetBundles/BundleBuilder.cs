1aa2cbfb6d61f6af2c3d8b34c7b25435174dff14
Yuri Green <yurigreen@gmail.com>
2016-05-16 01:32:47 -0700

    Adding new stuff

---
+++ AssetBundles/BundleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+using UnityEngione;
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("AssetBundles");
+    }
+}
---
+++ AssetBundles/GameMain.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMain : MonoBehavior
+{
+    public string bundleURL;
+    public RawImage questionImage;
+    public RawImage[] answerImage;
+
+    AssetBundle assetBundle;
+    int correctAnswer;
+
+    public void LoadQuestion ()
+    {
+        if (!assetBundle)
+        {
+
+        }
+    }
+
+    IEnumerator LoadAssetBundle (int questionNumber)
+    {
+        WWW www = new WWW(bundleURL);
+        yield return www;
+        assetBundle = www.assetBundle;
+        LoadQuestion(questionNumber);
+    }
+}
