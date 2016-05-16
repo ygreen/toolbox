6f4fecdda1c111d662f4350bcb8a2a5a42a10233
Yuri Green <yurigreen@gmail.com>
2016-05-16 07:03:33 -0700

    Added last details to GameMain

--- AssetBundles/GameMain.cs
+++ AssetBundles/GameMain.cs
@@ -6,17 +6,19 @@ public class GameMain : MonoBehavior
 {
     public string bundleURL;
     public RawImage questionImage;
-    public RawImage[] answerImage;
+    public RawImage[] answerImages;
 
     AssetBundle assetBundle;
     int correctAnswer;
 
-    public void LoadQuestion ()
+    public void LoadQuestion (int questNumber)
     {
         if (!assetBundle)
         {
-
+            StartCoroutine(LoadAssetBundle(questNumber));
+            return;
         }
+        StartCoroutine(ShowQuest(questNumber));
     }
 
     IEnumerator LoadAssetBundle (int questionNumber)
@@ -25,5 +27,23 @@ public class GameMain : MonoBehavior
         yield return www;
         assetBundle = www.assetBundle;
         LoadQuestion(questionNumber);
+    }
+
+    IEnumerator ShowQuest (int questionNumber)
+    {
+        AssetBundleRequest assetTexture = assetBundle.LoadAssetAsync<Texture2D>("p" + questionNumber);
+        yield return assetTexture;
+        questionImage.texture = assetTexture.asset as Texture2D;
+
+        for(int i=0; i < answerImages.Length; i++)
+        {
+            AssetBundleRequest assetAnswer = assetBundle.LoadAssetAsync<Texture2D>("J" + questionNumber + "-" + i);
+            yield return assetTexture;
+            answerImages[i].texture = assetAnswer.asset as Texture2D;
+        }
+
+        AssetBundleRequest assetCorrectAnswer = assetBundle.LoadAssetAsync<TextAsset>("Con" + questionNumber);
+        yield return assetCorrectAnswer;
+        correctAnswer = int.Parse((assetCorrectAnswer.asset as TextAsset).text);
     }
 }
